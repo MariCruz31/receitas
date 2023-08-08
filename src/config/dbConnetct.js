5ab135d7cc1fe69d2fcab6368cb92150c429676a
MariCruz31 <maricruzdev@gmail.com>
2023-08-08 10:56:56 -0300

    Conectando com o banco de dados

---
+++ src/config/dbConnetct.js
@@ -0,0 +1,10 @@
+// mongodb+srv://cruzmariene124:verde@cluster0.6djwvq1.mongodb.net/receitas
+
+import mongoose from "mongoose";
+
+mongoose.connect(
+     "mongodb+srv://cruzmariene124:verde@cluster0.6djwvq1.mongodb.net/receitas"  
+);
+let db = mongoose.connection;
+
+export default db;
